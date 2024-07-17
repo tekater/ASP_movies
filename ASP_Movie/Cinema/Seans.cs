0721615224429712ca7a5795f6f6db319d36ec86
tekater <tekateplay1@yandex.ru>
2024-07-17 12:31:47 +0900

    1.

--- ASP_Movie/Cinema/Seans.cs
+++ ASP_Movie/Cinema/Seans.cs
@@ -4,21 +4,20 @@ namespace ASP_Movie.Cinema
 {
 	public class Seans
 	{
-		public List<Movie> _films;
 			
-            static readonly Movie Movie1 = 
+            static readonly Movie MovieVenom = 
 			new Movie(
 				"Venom 5", 
 				"Director",
 				"Comedy",
                 "alien symbiote with an amorphous, liquid-like form, who survives by bonding with a host, usually human");
-			static readonly Movie Movie2 = 
+			static readonly Movie MovieSpider = 
 			new Movie(
                 "Spider-Man: The destroyer of souls", 
 				"Director", 
 				"Fantasy",
                 "Spider-Man gets his superhuman spider powers and abilities after being bitten by a radioactive spider");
-			static readonly Movie Movie3 = 
+			static readonly Movie MovieJoker = 
 			new Movie(
                 "Joker: Folie à Deux", 
 				"Director", 
@@ -26,29 +25,29 @@ namespace ASP_Movie.Cinema
                 "Failed comedian Arthur Fleck meets the love of his life, Harley Quinn, while incarcerated at Arkham State Hospita");
 			static readonly IDictionary<string, Movie> MovieList = new Dictionary<string, Movie>
 			   {
-				   {Movie1.Name, Movie1},
-				   {Movie2.Name, Movie2},
-				   {Movie3.Name, Movie3}
+				   {MovieVenom.Name, MovieVenom},
+				   {MovieSpider.Name, MovieSpider},
+				   {MovieJoker.Name, MovieJoker}
 			   };
 
 			public readonly IDictionary<string, List<ShowTime>> SessionList = new Dictionary<string, List<ShowTime>>
 			   {
 				   {
-					Movie1.Name, 
+					MovieVenom.Name, 
 					new List<ShowTime>{
 						new ShowTime("01.01.2026", "00:00"), 
 						new ShowTime("02.02.2026", "00:00")
 					}
 				},
 				   {
-					Movie2.Name, 
+					MovieSpider.Name, 
 					new List<ShowTime>{
 						new ShowTime("25.11.2027", "18:00"), 
 						new ShowTime("28.11.2027", "18:00")
 					}
 				},
 				   {
-					Movie3.Name, 
+					MovieJoker.Name, 
 					new List<ShowTime>{
 						new ShowTime("12.12.2025", "10:00"), 
 						new ShowTime("15.12.2025", "12:00")
@@ -57,8 +56,8 @@ namespace ASP_Movie.Cinema
 			   };
 			public Movie GetMovie(string name)
 			{
-				MovieList.TryGetValue(name, out Movie movie);
-				return movie;
+				MovieList.TryGetValue(name, out Movie movies);
+				return movies;
 			}
 		
 	}
